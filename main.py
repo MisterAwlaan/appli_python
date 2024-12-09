a71a2eb49805d998a39c731169ffeb87dadbb056
MonsieurPrime <nidri@guardiaschool.fr>
2024-12-09 12:56:53 +0100

    ajout des fonctionnalité des menus

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+def afficher_menu():
+    print("  Menu ")
+    print("1-Afficher la liste")
+    print("2-Ajout d'un produit")
+    print("3-suppression d'un produit")
 
-### Menu ###
 def afficher_liste():
-    liste = open("liste.txt","r",encoding="utf8")
-    line = liste.read()
-    return line
+    with open('./text/liste.txt','r',encoding='utf-8') as liste :
+        contenu = liste.read()
+        return contenu
+    
+def ajout_produit():
+    contenu = input("Nouveau produit : produit:prix   ")
+    with open('./text/liste.txt','a') as liste :
+        liste.write(contenu)
 
+def suppression_produit():
+    pass
+    
+    
+print(ajout_produit())
 print(afficher_liste())
