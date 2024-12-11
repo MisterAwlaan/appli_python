c66b41c1cdeae0327d35d81ac4a058907050425f
MonsieurPrime <nidri@guardiaschool.fr>
2024-12-11 11:49:42 +0100

    modification de toute les fonctions pour la quantité et le prix des produits

--- main.py
+++ main.py
@@ -3,31 +3,61 @@ def afficher_menu():
     print("1-Afficher la liste")
     print("2-Ajout d'un produit")
     print("3-suppression d'un produit")
-    print("4-quitter")
-    
+    print("4-rechercher un produit")
+    print("5-Trié le produit")    
+    print("6-Quitter")
+
+
 def afficher_liste():
     with open('./text/liste.txt','r',encoding='utf-8') as liste :
         contenu = liste.read()
         return contenu
     
 def ajout_produit():
-    contenu = input("Nouveau produit : ")
-    with open('./text/liste.txt','a') as liste :
-        liste.write(contenu + '\n')
+    nom = input("entrer le nom du produit : ")
+    quantite = input("entrer la quantité du produit : ")
+    prix = input("entrer le nom du produi : ")
+    with open('./text/liste.txt','a',encoding='utf-8') as liste :
+        liste.write(f"{nom};{quantite};{prix}\n")
+    print(f"Produit'{nom}'ajouté avec succès.")
 
 def suppression_produit():
-    ligne_a_supprimer = input("Entrez la ligne à supprimer (produit:prix) : ")
-    with open('./text/liste.txt', 'r') as liste:
-        lignes = liste.readlines()  
-    lignes_modifiees = [ligne for ligne in lignes if ligne.strip() != ligne_a_supprimer.strip()]
-    with open('./text/liste.txt', 'w') as liste:
-        liste.writelines(lignes_modifiees)  
-    print("La ligne a été supprimée.")
+    afficher_liste()
+    produit_a_supprimer = input("Quel produit à supprimer : ")
+    with open('./text/liste.txt','r',encoding='utf-8') as liste : 
+        produits = liste.readlines()
+    produits = [p for p in produits if not p.startswith(produit_a_supprimer+";")]
+    with open('./text/liste.txt','w',encoding='utf-8') as liste:
+        liste.writelines(produits)
+    print(f"Produit'{produit_a_supprimer}'supprimé avec succès") 
 
-def tri_produit_prix_croissant():
+    
     pass
 
+def tri_par_ordre_alphabetique():
+    with open('./text/liste.txt','r',encoding='utf-8') as liste :
+        lignes = liste.readlines()
+        tri = sorted([ligne.strip() for ligne in lignes ])
+    with open('./text/liste.txt','w') as fichier : 
+        for ligne in tri : 
+            fichier.write(ligne + '\n')
+    
+
 def recherche_produit():
+    nom_recherche = input("Entrer le nom du produit : ")
+    with open('./text/liste.txt','r',encoding='utf-8') as liste :
+        produits = liste.readlines()
+    produit_trouve = False
+    for produit in produits : 
+        nom,quantite,prix = produit.strip().split(';')
+        if nom_recherche.lower() in nom.lower():
+            print(f"Produit trouvé:Nom:{nom},Quantité:{quantite},Prix:{prix}€")
+            produit_trouve = True
+        if not produit_trouve:
+            print(f"Aucun produit correspond à '{nom_recherche}'trouvé.")
+    
+    
+    
     pass
     
 ####Script###
@@ -35,7 +65,7 @@ def recherche_produit():
 afficher_menu()
 choix = int(input("Bienvenue dans le menu veuillez choisir votre sélection parmi les 3 propositions : "))
 
-while choix != 4 : 
+while choix != 6 : 
     
     if choix == 1 : 
         print(afficher_liste())
@@ -53,5 +83,13 @@ while choix != 4 :
         afficher_menu()
         choix = int(input("Veuillez choisir votre sélection parmi les 3 propositions : "))
     if choix == 4 : 
+        recherche_produit()
+        print("")
+        choix = int(input("Veuillez choisir votre sélection parmi les 3 propositions : "))
+    if choix == 5 : 
+        tri_par_ordre_alphabetique()
+        print("")
+        choix = int(input("Veuillez choisir votre sélection parmi les 3 propositions : "))
+    if choix == 6 : 
         print("Aurevoir")
     
